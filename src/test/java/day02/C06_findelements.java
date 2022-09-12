f0fb7b95aa3b6fd1a358ae59bded2b158bd18617
Furkan Kenger <kengermf@gmail.com>
2022-09-12 21:46:34 +0300

    V1

--- src/test/java/day02/C06_findelements.java
+++ src/test/java/day02/C06_findelements.java
@@ -11,10 +11,12 @@ public class C06_findelements {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        // Amazon sayfasına gidiniz.
-        driver.get("https://amazon.com");
-        // Amazon sayfasındaki tag'ları link(a) olanların sayısını yazdırınız.
+        // Amazon sayfasına gidiniz
+        driver.get("https://www.amazon.com");
+        // Amazon sayfasındaki tag'ları input olanların sayısını yazdırınız
         List<WebElement> tag = driver.findElements(By.tagName("input"));
         System.out.println("input tag'inin sayısı = "+tag.size());
+
+        System.out.println(driver.findElement(By.xpath("(//*[@class='a-color-base headline truncate-2line'])[1]")).getText());
     }
 }
